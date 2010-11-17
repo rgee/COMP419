1af6715f6387767945f04260a8ec8d609670c8c8
Ryan Gee <rgee@wesleyan.edu>
2010-11-17 17:24:15 -0500

    Added axis-aligned bounding boxes. Currently static, if we need to allow them to be moved, that's trivial, so add it if you want it.

--- source/aabb.cpp
+++ source/aabb.cpp
@@ -3,5 +3,19 @@
 AABB::AABB(float x1, float y1, float x2, float y2)
 	:b_left(x1, y1), t_right(x2, y2)
 {
+}
 
+AABB::AABB(const CIwFVec2& bottom_left, const CIwFVec2& top_right)
+	:b_left(bottom_left), t_right(top_right)
+{
+}
+
+bool AABB::Intersect(float x, float y)
+{
+	return ((b_left.x < x) && (b_left.y > y)) && ((t_right.x > x) && (t_right.y < y));
+}
+
+bool AABB::Intersect(const CIwFVec2& point)
+{
+	return ((b_left.x < point.x) && (b_left.y > point.y)) && ((t_right.x > point.x) && (t_right.y < point.y));
 }
--- source/aabb.h
+++ source/aabb.h
@@ -6,15 +6,22 @@
 class AABB {
 public:
 	AABB(float x1, float y1, float x2, float y2);
-	AABB(const cIwSVec2& bottom_left, const cIwSVec2& top_right);
+	AABB(const CIwFVec2& bottom_left, const CIwFVec2& top_right);
 
 	~AABB();
 
+	/**
+	 * Intersect this bounding volume with a point (x,y)
+	 */
 	bool Intersect(float x, float y);
-	bool Intersect(const cIwSVec2& point);
+
+	/**
+	 * Intersect this bounding volume with a point.
+	 */
+	bool Intersect(const CIwFVec2& point);
 private:
-	cIwSVec2 b_left;
-	cIwSVec2 t_right;
+	CIwFVec2 b_left;
+	CIwFVec2 t_right;
 };
 
 #endif
