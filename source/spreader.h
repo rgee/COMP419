43497effe510a6843cb9d891ddda5f56e3ad20f5
rgee <rgee@wesleyan.edu>
2010-12-13 08:30:09 -0500

    Spreaders now spreading one icing at a time in pseudo random positions in a radius around them.

--- source/spreader.h
+++ source/spreader.h
@@ -1,14 +1,32 @@
 #ifndef _SPREADER_H
 #define _SPREADER_H
 
+#include <stdlib.h>
+#include <hash_map>
+#include <utility>
+
 #include "unit.h"
+#include "icing.h"
+#include <cmath>
 
 class Spreader : public Unit {
     private:
-	
-        int amountSpread;
+        // The maximum radii at which to drop icing.
+	    int maxSpread;
+
+        // The number of ticks since the last icing drop.
+        int sinceLastSpread;
+
+        // The delay between dropping an icing.
 		int spreadDelay;
+        
+        // The maximum number of individual icing to drop. Precomputed at construction.
+        int maxIcingCount;
+
 		CIwFVec2 worldRad;
+
+        // Map from (r, theta) centered at the spreader's center to icing pointers.
+        std::map<std::pair<int, float >, Icing*> icingMap;
     
 	public:
 		Spreader(Player* owner, Game* game, float x, float y);
@@ -20,6 +38,7 @@ class Spreader : public Unit {
 		virtual unit_type getType();
 		virtual Unit* spawnCopy();
         virtual bool shouldAIUpdate();
+        std::map<std::pair<int, float>, Icing*>* getIcing();
 };
 
 #endif
